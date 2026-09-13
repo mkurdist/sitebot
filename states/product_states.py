28d29ec4754d282ed8ef24da88b2c80d7e044e26
mka13 <176202317+mkurdist@users.noreply.github.com>
2026-09-13 23:26:45 +0300

    Update product_states.py

--- states/product_states.py
+++ states/product_states.py
@@ -1,7 +1,11 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class ProductWizard(StatesGroup):
-    name = State()
-    short_description = State()
-    price = State()
-    stock = State()
+    # دریافت نام اولیه
+    waiting_for_name = State()
+    
+    # وضعیت‌های مربوط به ویرایش از طریق داشبورد
+    waiting_for_price_stock = State()
+    waiting_for_seo = State()
+    waiting_for_desc = State()
+    # (بخش عکس و دسته‌بندی را در مراحل بعدی اضافه می‌کنیم)
