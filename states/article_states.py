89ae2aae926f2f71613fd056b1201cf280e32f30
mka13 <176202317+mkurdist@users.noreply.github.com>
2026-09-14 23:54:37 +0300

    Update article_states.py

--- states/article_states.py
+++ states/article_states.py
@@ -8,3 +8,7 @@ class ArticleWizard(StatesGroup):
     waiting_for_categories = State()
     waiting_for_primary_category = State()
     waiting_for_publish_action = State()
+
+    # وضعیت‌های ویرایش مقاله‌ی موجود
+    waiting_for_edit_title = State()
+    waiting_for_edit_content = State()
