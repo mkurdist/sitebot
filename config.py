4a934f64fd7fbba336a9af453cf9a2140c2aceec
mka13 <176202317+mkurdist@users.noreply.github.com>
2026-09-14 18:41:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,10 @@ ADMIN_ID = os.getenv("ADMIN_ID")
 WC_URL = os.getenv("WC_URL")
 WC_CONSUMER_KEY = os.getenv("WC_CONSUMER_KEY")
 WC_CONSUMER_SECRET = os.getenv("WC_CONSUMER_SECRET")
+WC_WEBHOOK_SECRET = os.getenv("WC_WEBHOOK_SECRET")  # <--- کلید امنیتی وب‌هوک اضافه شد
 
-# بررسی وجود متغیرها
-if not all([BOT_TOKEN, ADMIN_ID, WC_URL, WC_CONSUMER_KEY, WC_CONSUMER_SECRET]):
+# بررسی وجود متغیرها (وب‌هوک سکرت هم به بررسی اضافه شد)
+if not all([BOT_TOKEN, ADMIN_ID, WC_URL, WC_CONSUMER_KEY, WC_CONSUMER_SECRET, WC_WEBHOOK_SECRET]):
     raise ValueError("❌ Missing environment variables! Check Render settings.")
 
 ADMIN_ID = int(ADMIN_ID)
